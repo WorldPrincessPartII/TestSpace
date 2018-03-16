2c40169a4502c81150b3b90c95041ce0a0ae65af
CFS20035603L\stk <steven.khoupongsy@uqconnect.edu.au>
2018-03-16 23:50:58 +1000

    finally!
    Basic functional gtfs stuff working

--- allFeed.py
+++ allFeed.py
@@ -6,8 +6,4 @@ response = urllib.urlopen('https://gtfsrt.api.translink.com.au/Feed/SEQ')
 feed.ParseFromString(response.read())
 for entity in feed.entity:
   if entity.HasField('trip_update'):
-      if entity.trip_update.trip.route_id.startswith('130'):
-          print entity.trip_update.trip.trip_id
-          print entity.trip_update.trip.route_id
-    
-    
+    print entity.trip_update

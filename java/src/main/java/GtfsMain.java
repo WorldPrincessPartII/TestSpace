5004e339da92bd5597031b2f86a47bce60df24ec
CFS20035603L\stk <steven.khoupongsy@uqconnect.edu.au>
2018-03-17 21:41:19 +1000

    Incremental updates

--- java/src/main/java/GtfsMain.java
+++ java/src/main/java/GtfsMain.java
@@ -5,7 +5,7 @@ public class GtfsMain {
 
     public static void main(String[] args) throws Exception{
         Scanner reader = new Scanner(System.in);  // Reading from System.in
-        System.out.println("Enter a stop: ");
+        System.out.println("11168, 5817. Enter a stop: ");
         String stop = reader.nextLine();
         reader.close();
         StopData.get(stop);

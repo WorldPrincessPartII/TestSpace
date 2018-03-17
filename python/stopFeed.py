bd9233aff5b90bfa5d711a58ceed565eeffc7407
CFS20035603L\stk <steven.khoupongsy@uqconnect.edu.au>
2018-03-17 10:53:36 +1000

    More python updates

--- python/stopFeed.py
+++ python/stopFeed.py
@@ -4,14 +4,18 @@
 from google.transit import gtfs_realtime_pb2
 import urllib
 import time
+from operator import itemgetter
 
 feed = gtfs_realtime_pb2.FeedMessage()
 response = urllib.urlopen('https://gtfsrt.api.translink.com.au/Feed/SEQ')
 feed.ParseFromString(response.read())
+
+arrivalList = []
+
 for entity in feed.entity:
   if entity.HasField('trip_update'):
     for stopUpdate in entity.trip_update.stop_time_update:
-      if stopUpdate.stop_id.startswith('10'): #'5817': ## stop number here
+      if stopUpdate.stop_id == '5817': ## stop number here
           #print entity.trip_update.trip.trip_id
           timeCurrent = int(time.time())
           timeArrival = stopUpdate.arrival.time
@@ -20,7 +24,17 @@ for entity in feed.entity:
             print entity.trip_update.trip.route_id
             fleetNo = entity.trip_update.vehicle.id.split('_')
             print '{} {}'.format('Bus number:', fleetNo[1])
-            print '{} {}'.format(timeGap/60, 'minutes to a stop')
+            stopNo = stopUpdate.stop_id
+            print '{} {} {}'.format(timeGap/60, 'minutes to stop', stopNo)
+
+            route = (str(fleetNo[1]), int(timeGap/60))
+            arrivalList.append(route)
+
+sorted(arrivalList,key=itemgetter(1))
+
+for i in arrivalList:
+  print i
+
           #print stopUpdate.stop_id
     
 ##      if entity.trip_update.trip.route_id.startswith('130'):
--- python/vehicleFeed.py
+++ python/vehicleFeed.py
@@ -15,8 +15,11 @@ for entity in feed.entity:
           print entity.trip_update.trip.route_id
           fleetNo = entity.trip_update.vehicle.id.split('_')
           print '{} {}'.format('Bus number:', fleetNo[1])
-          for stopUpdate in entity.trip_update.stop_time_update:
-            print stopUpdate.stop_id
+          f = open("seq.txt",'a')
+          f.write('{} {} \n'.format('Bus number:', fleetNo[1]))
+          f.close()
+          #for stopUpdate in entity.trip_update.stop_time_update:
+            #print stopUpdate.stop_id
             # Put into array, check array length
           
             
